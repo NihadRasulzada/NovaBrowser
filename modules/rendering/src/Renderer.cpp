02a54ef74a3f8381b82ee7c506fefd0fea57549d
NihadRasulzada <dev.nihad.rasulzada@outlook.com>
2026-09-07 16:58:37 +0300

    refactor: reorganize project modules and platform abstraction
    - Separate browser modules from framework modules
    - Move UI implementation under browser/ui
    - Introduce framework UI module
    - Add platform abstraction interface
    - Remove legacy rendering module
    - Remove URL lexer tests
    - Update CMake module structure

--- modules/rendering/src/Renderer.cpp
+++
@@ -1,9 +0,0 @@
-#include "rendering/Renderer.hpp"
-
-namespace NovaBrowser::Rendering {
-
-void Renderer::Render(PaintContext &context) {
-  // Hələlik test rendering
-}
-
-} // namespace NovaBrowser::Rendering
